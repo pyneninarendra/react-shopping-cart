5d1e9998de51c35d836af5cbe1a54f3082433c97
pyneninarendra <pyneninarendra@gmail.com>
2024-09-18 06:29:52 +0530

    product list component changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 //Feature - 1
+import React, { Component } from "react";
+import products from "./products.json";
+import Products from "./components/Products";
 
+export class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-        Products List
-      </main>
-      <footer>
-        All rights reserved, 2024
-      </footer>
-    </div>
-  );
+    this.state = {
+      products: products.products,
+      size: "",
+      sorts: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All rights reserved, 2024</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
